c2120a08d364e55656a0020edddf6d862a86b75e
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-11-29 19:16:21 -0700

    reorganized from previous

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Employee from './components/Employee.jsx'
+
+
+
+import Mockaroo from 'mockaroo'
+var client = new Mockaroo.Client({
+  apiKey: 'c01a9820'
+})
+
+class App extends Component {
+  state = {
+    data: []
+  }
+  click = () => {
+    client.generate({
+      count: 10,
+      schema: 'Employees'
+    }).then(res => {
+      console.log(res)
+      const mockData = res.map(data => ({
+        id: data.id,
+        fName: data.first_name,
+        lName: data.last_name,
+        title: data.title,
+        email: data.email
+      }))
+      this.setState({ data: mockData })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hi from App</p>
+        { this.state.data.map(employee => < Employee key={employee.id}
+          fName={employee.fName}
+          lName={employee.lName}
+          title={employee.title}
+          email={employee.email}
+        />)}
+        <button onClick={this.click}>click</button>
+      </div >
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Employee.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Employee(props) {
+    // console.log(props)
+    return (
+        <div>
+            <h2>First Name: {props.fName}</h2>
+            <h2>Last Name: {props.lName}</h2>
+            <h2>Title: {props.title}</h2>
+            <h2>Email: {props.email}</h2>
+        </div>
+    )
+};
+
+export default Employee;
