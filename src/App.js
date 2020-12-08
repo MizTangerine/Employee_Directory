78ae23be49a4488e0463b97afe333a78b844595c
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-12-07 22:05:55 -0700

    sort is working

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import './App.css';
-// import Employee from './components/Employee.jsx'
+// import './App.css';
 import Header from './components/Header.jsx'
-import { Table } from './components/Table.jsx'
+import employees from './employees.json'
+import Employee from './components/Employee.jsx';
 
-// import { MOCKAROO_KEY } from "@env"
-// import Mockaroo from 'mockaroo'
-// var client = new Mockaroo.Client({
-//   apiKey: 'c01a9820'
-// })
+export default function App() {
 
-class App extends Component {
-  state = {
-    data: []
-  }
-  // click = () => {
-  //   client.generate({
-  //     count: 10,
-  //     schema: 'Employees'
-  //   }).then(res => {
-  //     console.log(res)
-  //     const mockData = res.map(data => ({
-  //       id: data.id,
-  //       fName: data.first_name,
-  //       lName: data.last_name,
-  //       title: data.title,
-  //       email: data.email,
-  //       avatar: data.avatar
-  //     }))
-  //     this.setState({ data: mockData })
-  //   });
-  // }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Table />
-        {/* <p>Hi from App</p>
-        { this.state.data.map(employee => < Employee key={employee.id}
-          avatar={employee.avatar}
-          fName={employee.fName}
-          lName={employee.lName}
-          title={employee.title}
-          email={employee.email}
-        />)} */}
-        {/* <button onClick={this.click}>click</button> */}
-      </div >
-    );
-  }
+  return (
+    <div>
+      <Header />
+      <Employee employees={employees} />
+    </div >
+  );
 }
-
-export default App;
--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,16 +1,61 @@
-// import React from "react";
+import React from 'react';
+import useSortableData from './Sort'
 
-// function Employee(props) {
-//     // console.log(props)
-//     return (
-//         <div>
-//             <img src={props.avatar} alt={props.fName}></img>
-//             <h2>First Name: {props.fName}</h2>
-//             <h2>Last Name: {props.lName}</h2>
-//             <h2>Title: {props.title}</h2>
-//             <h2>Email: {props.email}</h2>
-//         </div>
-//     )
-// };
 
-// export default Employee;
+const Employee = (props) => {
+    const { employees, requestSort, sortConfig } = useSortableData(props.employees);
+    const getClassNamesFor = (name) => {
+        if (!sortConfig) {
+            return;
+        }
+        return sortConfig.key === name ? sortConfig.direction : undefined;
+    };
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        <button
+                            type="button"
+                            onClick={() => requestSort('first_name')}
+                            className={getClassNamesFor('first_name')}
+                        >First Name</button>
+                    </th>
+                    <th>
+                        <button
+                            type="button"
+                            onClick={() => requestSort('last_name')}
+                            className={getClassNamesFor('last_name')}
+                        >Last Name</button>
+                    </th>
+                    <th>
+                        <button
+                            type="button"
+                            onClick={() => requestSort('email')}
+                            className={getClassNamesFor('email')}
+                        >Email</button>
+                    </th>
+                    <th>
+                        <button
+                            type="button"
+                            onClick={() => requestSort('phone')}
+                            className={getClassNamesFor('phone')}
+                        >Phone Number</button>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {employees.map((emp) => (
+                    <tr key={emp.id}>
+                        <td>{emp.first_name}</td>
+                        <td>{emp.last_name}</td>
+                        <td>{emp.email}</td>
+                        <td>{emp.phone}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default Employee
