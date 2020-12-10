152b765ae14360155f88278f825ba252e3641466
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-12-09 22:12:59 -0700

    everything is functional, styline is added

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import useSortableData from './Sort'
+import './Employee.css'
 
 
 const Employee = (props) => {
@@ -7,86 +8,87 @@ const Employee = (props) => {
     const { employees, requestSort, sortConfig } = useSortableData(props.employees);
     const getClassNamesFor = (name) => {
         if (!sortConfig) {
-            return;
+            return 'sort';
         }
-        return sortConfig.key === name ? sortConfig.direction : undefined;
+        return sortConfig.key === name ? sortConfig.direction : 'sort';
     };
     return (
-        <table>
-
-            <thead>
-                <tr>
-                    <th>
-                        <input
-                            type="text"
-                            value={value}
-                            onChange={e => setValue(e.target.value)}
-                        />
-                    </th>
-                </tr>
-                <tr>
-                    <th>
-                        <button
-                            type="button"
-                            onClick={() => requestSort('first_name')}
-                            className={getClassNamesFor('first_name')}
-                        >First Name</button>
-                    </th>
-                    <th>
-                        <button
-                            type="button"
-                            onClick={() => requestSort('last_name')}
-                            className={getClassNamesFor('last_name')}
-                        >Last Name</button>
-                    </th>
-                    <th>
-                        <button
-                            type="button"
-                            onClick={() => requestSort('title')}
-                            className={getClassNamesFor('title')}
-                        >Title</button>
-                    </th>
-                    <th>
-                        <button
-                            type="button"
-                            onClick={() => requestSort('email')}
-                            className={getClassNamesFor('email')}
-                        >Email</button>
-                    </th>
-                    <th>
-                        <button
-                            type="button"
-                            onClick={() => requestSort('phone')}
-                            className={getClassNamesFor('phone')}
-                        >Phone Number</button>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {employees
-                    .filter(item => {
-                        if (!value) return true;
-                        if (item.title.toLowerCase().includes(value.toLowerCase())
-                            || item.first_name.toLowerCase().includes(value.toLowerCase())
-                            || item.last_name.toLowerCase().includes(value.toLowerCase())
-                            || item.email.toLowerCase().includes(value.toLowerCase())
-                            || item.phone.toLowerCase().includes(value.toLowerCase())
-                        ) {
-                            return true;
-                        }
-                        return false;
-                    })
-                    .map((emp) => (
-                        <tr key={emp.id}>
-                            <td>{emp.first_name}</td>
-                            <td>{emp.last_name}</td>
-                            <td>{emp.title}</td>
-                            <td>{emp.email}</td>
-                            <td>{emp.phone}</td>
-                        </tr>
-                    ))}
-            </tbody>
-        </table>
+        <>
+            <input
+                type="text"
+                autoComplete="off"
+                placeholder="Enter Search Criteria Here"
+                value={value}
+                onChange={e => setValue(e.target.value)}
+            />
+            <table>
+                <thead>
+                    <tr>
+                        {/* <th>Avatar</th> */}
+                        <th>
+                            <button
+                                type="button"
+                                onClick={() => requestSort('first_name')}
+                                className={getClassNamesFor('first_name')}
+                            >First Name</button>
+                        </th>
+                        <th>
+                            <button
+                                type="button"
+                                onClick={() => requestSort('last_name')}
+                                className={getClassNamesFor('last_name')}
+                            >Last Name</button>
+                        </th>
+                        <th>
+                            <button
+                                type="button"
+                                onClick={() => requestSort('title')}
+                                className={getClassNamesFor('title')}
+                            >Title</button>
+                        </th>
+                        <th>
+                            <button
+                                type="button"
+                                onClick={() => requestSort('email')}
+                                className={getClassNamesFor('email')}
+                            >Email</button>
+                        </th>
+                        <th>
+                            <button
+                                type="button"
+                                onClick={() => requestSort('phone')}
+                                className={getClassNamesFor('phone')}
+                            >Phone Number</button>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees
+                        .filter(item => {
+                            if (!value) return true;
+                            if (item.title.toLowerCase().includes(value.toLowerCase())
+                                || item.first_name.toLowerCase().includes(value.toLowerCase())
+                                || item.last_name.toLowerCase().includes(value.toLowerCase())
+                                || item.email.toLowerCase().includes(value.toLowerCase())
+                                || item.phone.toLowerCase().includes(value.toLowerCase())
+                            ) {
+                                return true;
+                            }
+                            return false;
+                        })
+                        .map((emp) => (
+                            <tr key={emp.id}>
+                                {/* <td><img src={emp.avatar} alt="avatar" /></td> */}
+                                <td>{emp.first_name}</td>
+                                <td>{emp.last_name}</td>
+                                <td>{emp.title}</td>
+                                <td>{emp.email}</td>
+                                <td className='phone'>{emp.phone}</td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+        </>
     );
 };
 
