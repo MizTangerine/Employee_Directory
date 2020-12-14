7c0f5f3d6700ba0e5d6039dc1b049323a0a6987e
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-12-13 18:14:41 -0700

    removed comments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-// import './App.css';
 import Header from './components/Header.jsx'
 import employees from './employees.json'
 import Employee from './components/Employee.jsx';
-// import Filter from './components/Filter.jsx'
 
 export default function App() {
-
   return (
     <div>
       <Header />
-      {/* <Filter /> */}
       <Employee employees={employees} />
     </div >
   );
